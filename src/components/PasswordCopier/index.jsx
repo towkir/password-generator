1b78fd658893af2c2bcbc188462879261f5f6a59
Towkir Ahmed <towkir17@gmail.com>
2024-03-21 02:26:49 +0600

    added function to copy the generated password

--- src/components/PasswordCopier/index.jsx
+++ src/components/PasswordCopier/index.jsx
@@ -1,11 +1,23 @@
 import copyIcon from 'src/assets/icons/icon-copy.svg'
 import './index.css'
+import { useState } from 'react'
 
 function PasswordCopier({ password }) {
+  const [copied, setCopied] = useState(false)
+
+  function copyPassword() {
+    navigator.clipboard.writeText(password);
+    setCopied(true)
+      setTimeout(() => {
+      setCopied(false)
+    }, 300);
+  }
+  
   return (
     <div className="password-copier">
       <input type="text" value={password} placeholder="P4$5W0rD!" readOnly="readonly" disabled/>
-      <button type='button'>
+      <button type="button" className={`copy-btn ${copied ? 'copied' : ''}`}
+        disabled={password === ''} onClick={copyPassword}>
         <img src={copyIcon} />
       </button>
     </div>
